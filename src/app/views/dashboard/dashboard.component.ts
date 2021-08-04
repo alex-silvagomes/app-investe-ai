5acfe3cb94069ae137053fd6a9149b33bfc3d789
Alex Gomes <alex.gomes@inmetrics.com.br>
2021-08-04 11:42:43 -0300

    update dashboard component get funds values

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -13,11 +13,24 @@ export class DashboardComponent implements OnInit {
   // funds List
   funds: Array<any> = dataset;
   
+  funds_owned: Array<any> = this.funds.filter(function(fund) {
+    return fund.owned == true;
+  });
+
+  funds_custo_total: number = 0;
+  // funds_rentabilidade_prevista: number = this.funds_owned.reduce((accumulator, current) => accumulator + (current.cotas+)), 0);
+  //funds_rentabilidade_prevista: number = this.funds_owned.reduce(function (acc, cur) { return acc + Number(cur.cotas); });
+ 
+
+
   constructor( ) {}
 
   ngOnInit(): void {
     
-    console.log("Load", this.funds);
-    
+    console.log("Load:", this.funds);
+    console.log("Minhas Cotas:", this.funds_owned);
+    console.log("Custo Total:", this.funds_custo_total);
+    // console.log("Rentabilidade Prevista:", this.funds_rentabilidade_prevista);
+
   }
 }
